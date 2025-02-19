4d768d7add0ffb2baf61fa27cf8dd3d0642a49e5
onewayay <onewayay@naver.com>
2025-02-19 14:18:23 +0900

    2025-02-19
    -폼 (button 요소)
    -폼 (select, textarea 요소)
    -dialog 요소, popover 요소
    -details, summary 요소
    -script 요소

---
+++ src/html/js/23-script.js
@@ -0,0 +1,17 @@
+document.querySelector('.flip-button').addEventListener('click', () => {
+  document.querySelector('.card').classList.toggle('is-flipped');
+});
+
+/*
+// DOM 선택 부분
+const flipButton = document.querySelector('.flip-button');
+const card = document.querySelector('.card');
+
+// 함수 구현 부분
+const flipCard = () => {
+  card.classList.toggle('is-flipped');
+};
+
+// 이벤트 바인딩 부분
+flipButton.addEventListener('click', flipCard);
+*/
